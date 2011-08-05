0d91a66cc7833215eee622d7f39bb53c798598b2
brez <jbresnik@gmail.com>
2011-08-05 16:27:54 -0400

    updating test

--- examples/test.js
+++ examples/test.js
@@ -1,48 +1,23 @@
 var Face = require('../build/default/face.node'),
-	recognizer = new Face.init(),
-	Canvas = require('../deps/canvas/canvas.js'),
-	Image = Canvas.Image,
-	canvas = new Canvas(800, 600),
-	ctx = canvas.getContext('2d');
-
-
-function boundFaces(input, faces) {
-	var img = new Image;
-
-	img.onload = function(){
-		ctx.drawImage(img, 0, 0);
-	}
-
-	img.src = input;
-
-	ctx.strokeStyle = 'rgb(255, 0, 0)';
-	for(var i = 0; i < faces.length; i++) {
-		ctx.strokeRect(faces[i].x, faces[i].y, faces[i].width, faces[i].height);
-	}
-}
-
+  recognizer = new Face.init();
 
 function getFaces(res) {
-	recognizer.img = '/home/sebastian/mtv.png';
-
-	recognizer.oncomplete = function(faces){
-
-		boundFaces(recognizer.img, faces);
-		res.write("I found " + faces.length + " faces <BR>");
-		res.end('<img src="' + canvas.toDataURL() + '" />');
-
-	};
-
-	recognizer.run();
-	
+  recognizer.img = '/Users/jbresnik/Desktop/hr/john.jpg';
+  console.log('In getFaces');
+  recognizer.oncomplete = function(faces){
+    console.log('In oncomplete');
+    res.write("I found " + faces.length + " faces <BR>");
+    res.end('Hello World\n');
+  };
+  recognizer.run();
 }
 
 var http = require('http');
 http.createServer(function (req, res) {
-				res.writeHead(200, { 'Content-Type': 'text/html' });
-				res.write("Getting faces...<BR>");
-				getFaces(res);
-				
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write("Getting faces...<BR>");
+        getFaces(res);
+
 }).listen(3000);
 
 console.log('Server started on port 3000');
